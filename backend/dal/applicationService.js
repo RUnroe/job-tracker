cd4e7a8bb4f4db8e39bdf0600e9d00160030ebdc
Ryan Unroe <ryanunroe@gmail.com>
2024-11-25 16:59:16 -0700

    Build out data access layer

--- backend/dal/applicationService.js
+++ backend/dal/applicationService.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const mongo = require("mongodb");
 
 
@@ -11,6 +12,46 @@ dbClient.connect().then(() => console.log("Connected to Mongo database")).catch(
 
 const applicationService = {
 
+  getAllApplications: async function() {
+    try {
+      const applicationList = await dbClient.db(process.env.DATABASE_NAME).collection("application").find().toArray();
+      console.log(applicationList);
+      return applicationList;
+    }
+    catch (ex) {
+      const errorMessage = `Error querying for application list: ${ex.message}`;
+      console.error(errorMessage);
+      return null;
+    }
+  },
+
+  getApplicationById: async function(id) {
+    try {
+      const applicationById = await dbClient.db(process.env.DATABASE_NAME).collection("application").findOne({"_id": mongo.ObjectId(id)});
+      console.log(applicationById);
+      return applicationById;
+    }
+    catch (ex) {
+      const errorMessage = `Error querying for application by id: ${ex.message}`;
+      console.error(errorMessage);
+      return null;
+    }
+  },
+
+  createApplication: async function(application, userId) {
+    application.userId = userId;
+    try {
+      const result = await dbClient.db(process.env.DATABASE_NAME).collection("application").insertOne(application);
+      console.log(result);
+      return result.insertedId;
+    }
+    catch (ex) {
+      const errorMessage = `Error creating application: ${ex.message}`;
+      console.error(errorMessage);
+      return null;
+    }
+  },
+
   getAllTechnologies: async function() {
     try {
       const technologies = await dbClient.db(process.env.DATABASE_NAME).collection("technology").find().toArray();
@@ -22,7 +63,7 @@ const applicationService = {
       console.error(errorMessage);
       return null;
     }
-  }
+  },
 
 }
 
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,23 +4,47 @@ const { applicationService } = require('../dal/applicationService');
 
 const signInUrl = "https://natural-tick-28.accounts.dev/sign-in";
 
-const configure = (app, dbClient) => {
+const configure = (app) => {
   app.get('/application/list', clerk.requireAuth({ signInUrl }), async (req, res) => {
     const { userId } = req.auth;
     const user = await clerk.clerkClient.users.getUser(userId);
-    return res.json({ user });
+    if(user) {
+      const applicationList = await applicationService.getAllApplications();
+      if(applicationList) {
+        return res.json(applicationList);
+      }
+      else {
+        res.status(500).send();
+      }
+    }
   });
 
   app.get('/application/:id', clerk.requireAuth({ signInUrl }), async (req, res) => {
     const { userId } = req.auth;
     const user = await clerk.clerkClient.users.getUser(userId);
-    return res.json({ user });
+    if(user) {
+      const application = await applicationService.getApplicationById(req.params.id);
+      if(application) {
+        return res.json(application);
+      }
+      else {
+        res.status(500).send();
+      }
+    }
   });
 
   app.post('/application/new', clerk.requireAuth({ signInUrl }), async (req, res) => {
     const { userId } = req.auth;
     const user = await clerk.clerkClient.users.getUser(userId);
-    return res.json({ user });
+    if(user) {
+      const newApplicationId = await applicationService.createApplication(req.body, userId);
+      if(newApplicationId) {
+        return res.json(newApplicationId);
+      }
+      else {
+        res.status(500).send();
+      }
+    }
   });
 
   // app.get('/options/technology', clerk.requireAuth({ signInUrl }), async (req, res) => {
@@ -28,10 +52,12 @@ const configure = (app, dbClient) => {
   //   const user = await clerk.clerkClient.users.getUser(userId);
     
   //   if(user) {
-  //     let technologies = await applicationService.getAllTechnologies();
-  //     return res.json(technologies);
+  //    let technologies = await applicationService.getAllTechnologies();
+      // if(technologies) {
+      //   return res.json(technologies);
+      // }
   //   }
-  //   return res.status(404).send();
+  //   res.status(500).send();
   // });
 
   app.get('/options/technology', async (req, res) => {
@@ -47,6 +73,7 @@ const configure = (app, dbClient) => {
   });
 
   app.get('/application/statistics', clerk.requireAuth({ signInUrl }), async (req, res) => {
+    //TODO
     const { userId } = req.auth;
     const user = await clerk.clerkClient.users.getUser(userId);
     return res.json({ user });
